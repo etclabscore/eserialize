6eb8456c6540b50fc522eaaa503d565513dbd12d
shanejonas <jonas.shane@gmail.com>
2020-05-12 00:03:04 -0700

    feat: add options for padding + prefix

--- src/serialize/numberToHex.test.ts
+++ src/serialize/numberToHex.test.ts
@@ -4,4 +4,14 @@ describe("numberToHex", () => {
   it("can convert number to hex string", () => {
     expect(numberToHex(16)).toEqual("0x10");
   });
+  it("can pad", () => {
+    const str = "foo";
+    const pad = 10;
+    expect(numberToHex(16, { padStart: pad })).toEqual("0x0000000010");
+  });
+  it("can pad with no 0x prefix", () => {
+    const str = "foo";
+    const pad = 10;
+    expect(numberToHex(16, { padStart: pad, noPrefix: true })).toEqual("0000000010");
+  });
 });
